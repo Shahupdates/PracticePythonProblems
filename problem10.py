b77d78794f5d7fc3848f18feb12504cfe610a28b
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-16 17:42:29 -0400

    Create problem10.py

---
+++ problem10.py
@@ -0,0 +1,16 @@
+"""
+Find the Square Root of a Number
+
+Write a program that takes a number as input and calculates its square root.
+"""
+
+import math
+
+# Take input from the user
+number = float(input("Enter a number: "))
+
+# Calculate the square root
+square_root = math.sqrt(number)
+
+# Print the result
+print("The square root of", number, "is", square_root)
